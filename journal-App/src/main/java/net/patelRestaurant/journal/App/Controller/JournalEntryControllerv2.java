84663313ce41857dd6dc483b9600b7f4c7f54c32
Rambharat Patel <80194177+patelrambharat@users.noreply.github.com>
2025-07-25 21:48:36 +0530

    contoller part is done.....

--- journal-App/src/main/java/net/patelRestaurant/journal/App/Controller/JournalEntryControllerv2.java
+++ journal-App/src/main/java/net/patelRestaurant/journal/App/Controller/JournalEntryControllerv2.java
@@ -9,30 +9,30 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/_journal")
-public class JournalEntryController {
+@RequestMapping("/journal")
+public class JournalEntryControllerv2 {
 
-            private Map<Long , JournalEntry> journalEntryMap = new HashMap<>();
+
             // GET API - Fetch all journal entries
             @GetMapping("/all")
             public List<JournalEntry> getAll(){
-                return new ArrayList<>(journalEntryMap.values());
+                return null;
             }
             // POST API - Add new journal entry
             @PostMapping("/add")
-            public void createEntry(@RequestBody JournalEntry myEntry){
-                journalEntryMap.put(myEntry.getId(), myEntry);
+            public boolean createEntry(@RequestBody JournalEntry myEntry){
+               return true;
             }
             @GetMapping("/id/{myId}")
             public JournalEntry getJournalEntryById(@PathVariable Long myId){
-                        return journalEntryMap.get(myId);
+                    return null;
             }
             @DeleteMapping("/id/{myId}")
             public JournalEntry deleteJournalEntryById(@PathVariable Long myId){
-                        return journalEntryMap.remove(myId);
+                    return null;
             }
             @PutMapping("/id/{id}")
             public JournalEntry updateJournalEntryById(@PathVariable Long id, @RequestBody JournalEntry myEntry){
-                return journalEntryMap.put(id, myEntry);
+                return null;
             }
 }
