ef20d67559cdfbdf29eb08d3c599dc15c593580f
Rambharat Patel <80194177+patelrambharat@users.noreply.github.com>
2025-08-02 20:04:34 +0530

    updated code for criteria

--- journal-App/src/main/java/net/patelRestaurant/journal/App/entity/User.java
+++ journal-App/src/main/java/net/patelRestaurant/journal/App/entity/User.java
@@ -1,8 +1,6 @@
 package net.patelRestaurant.journal.App.entity;
 
-import lombok.Builder;
-import lombok.Data;
-import lombok.NonNull;
+import lombok.*;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
@@ -16,18 +14,20 @@ import java.util.List;
 @Document(collection =  "users")
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     private ObjectId id;
     @Indexed(unique = true)
     @NonNull
     private String userName;
+    private String email;
+    private boolean sentimentAnalysis;
     @NonNull
     private String password;
-
     @DBRef
-    private List<JournalEntry> journalEntryList = new ArrayList<>();
-
-    private List<String>roles;
+    private List<JournalEntry> JournalEntryList = new ArrayList<>();
+    private List<String> roles;
 
 }
