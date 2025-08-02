ec803efb63a4edd403f6ba1eb8da76f89be97a15
Rambharat Patel <80194177+patelrambharat@users.noreply.github.com>
2025-08-02 16:17:08 +0530

    use @Value Annotation to save keys in yaml

--- journal-App/src/main/java/net/patelRestaurant/journal/App/Service/WeatherService.java
+++ journal-App/src/main/java/net/patelRestaurant/journal/App/Service/WeatherService.java
@@ -2,6 +2,7 @@ package net.patelRestaurant.journal.App.Service;
 
 import net.patelRestaurant.journal.App.api.response.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -9,7 +10,8 @@ import org.springframework.web.client.RestTemplate;
 
 @Component
 public class WeatherService {
-    private static final String apiKey = "";
+    @Value("${weather.api.key}")
+    private String apiKey;
 
     private static final String API = "http://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
 
@@ -18,7 +20,8 @@ public class WeatherService {
 
     public WeatherResponse getWeather(String city){
         String finalAPI = API.replace("CITY", city).replace("API_KEY", apiKey);
-        ResponseEntity<WeatherResponse> response = restTemplate.exchange(finalAPI, HttpMethod.GET, null,WeatherResponse.class);
+
+        ResponseEntity<WeatherResponse> response = restTemplate.exchange(finalAPI, HttpMethod.POST, null,WeatherResponse.class);
         WeatherResponse body = response.getBody();
         return body;
     }
